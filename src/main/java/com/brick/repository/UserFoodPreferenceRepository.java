dd7490a38df2e5d86ebe88764b9d8682e00658b4
Chaeyeoncho <gcy9293@naver.com>
2025-12-04 17:53:09 +0900

    feat : UserFoodPreferenceRepository.java

---
+++ src/main/java/com/brick/repository/UserFoodPreferenceRepository.java
@@ -0,0 +1,8 @@
+package com.brick.repository;
+
+import com.brick.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//public interface UserFoodPreferenceRepository extends JpaRepository<UserFoodPreference, Long> {
+//    List<UserFoodPreference> findByUser(User user);
+//}
