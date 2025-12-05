e89712b6224eea85864fb08f664d96488d015112
Chaeyeoncho <gcy9293@naver.com>
2025-12-05 16:27:58 +0900

    refactor : interface refactoring

--- src/main/java/com/brick/repository/UserFoodPreferenceRepository.java
+++ src/main/java/com/brick/repository/UserFoodPreferenceRepository.java
@@ -1,8 +1,11 @@
 package com.brick.repository;
 
 import com.brick.entity.User;
+import com.brick.entity.UserFoodPreference;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//public interface UserFoodPreferenceRepository extends JpaRepository<UserFoodPreference, Long> {
-//    List<UserFoodPreference> findByUser(User user);
-//}
+import java.util.List;
+
+public interface UserFoodPreferenceRepository extends JpaRepository<UserFoodPreference, Long> {
+    List<UserFoodPreference> findByUser(User user);
+}
--- src/main/java/com/brick/service/UserPreferenceService.java
+++ src/main/java/com/brick/service/UserPreferenceService.java
@@ -1,4 +1,17 @@
 package com.brick.service;
+import com.brick.dto.PreferenceResponseDto;
+import com.brick.dto.UserResponseDto;
 
-public class UserPreferenceService {
+import java.util.List;
+
+
+public interface UserPreferenceService {
+
+    // 저장
+    void saveUserPreferences(Long userId , List<Long> foodIds);
+
+    // 조회 ( 이때 responseDto 는 id가 포함 X , but 파라미터로 들어옴 )
+    List<PreferenceResponseDto> getUserPreferences(Long userId);
+
+
 }
