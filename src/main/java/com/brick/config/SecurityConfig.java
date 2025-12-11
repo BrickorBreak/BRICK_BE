1bed2f3a1352c69dd3cf8b2ccee09130c57e0eb6
Chaeyeoncho <gcy9293@naver.com>
2025-12-11 14:11:12 +0900

    fix : .anyRequest().permitAll()

--- src/main/java/com/brick/config/SecurityConfig.java
+++ src/main/java/com/brick/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.brick.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -12,19 +14,22 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())  //Talend 테스트용
                 .authorizeHttpRequests(auth -> auth
 
 //                        .requestMatchers("/api/v1/auth/**").permitAll() //회원가입/로그인은 인증 없이 허용
 
-                        .requestMatchers(
-                                "/api/v1/auth/**",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/swagger-ui.html"
-                        ).permitAll() //회원가입/로그인은 인증 없이 허용
-
-                        .anyRequest().authenticated()
+//                        .requestMatchers(
+//                                "/api/v1/auth/**",
+//                                "/swagger-ui/**",
+//                                "/v3/api-docs/**",
+//                                "/swagger-ui.html",
+//                                "/api/v1/feeds/**",
+//                                "/api/v1/comments/**",
+//                                "/swagger-resources/**",
+//                                "/api/v1/feeds/*/comments"
+                        .anyRequest().permitAll()
                 )
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .formLogin(form -> form.disable());
