48110c32b8f2cc7094c4371bfe6cd30474e7c1b0
Chaeyeoncho <gcy9293@naver.com>
2025-12-15 11:07:32 +0900

    fix : date 필드 추가

--- src/main/java/com/brick/dto/FeedImageDto.java
+++ src/main/java/com/brick/dto/FeedImageDto.java
@@ -3,9 +3,12 @@ package com.brick.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class FeedImageDto {
     private String imageUrl;
     private Integer sequence;
+    private LocalDateTime takenTime;
 }
