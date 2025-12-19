bf51bbda2a2c8cd430fde82eec0faa3123175ea3
Chaeyeoncho <gcy9293@naver.com>
2025-12-20 01:44:23 +0900

    fix : user

--- src/main/java/com/brick/service/UserService.java
+++ src/main/java/com/brick/service/UserService.java
@@ -2,10 +2,11 @@ package com.brick.service;
 
 import com.brick.dto.UserRequestDto;
 import com.brick.dto.UserResponseDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
     // 등록
-    void updateUser(Long userId , UserRequestDto dto);
+    void updateUser(Long userId , UserRequestDto dto , MultipartFile image);
 
     // 조회
     UserResponseDto getUser(Long userId);
--- src/main/java/com/brick/service/UserServiceImpl.java
+++ src/main/java/com/brick/service/UserServiceImpl.java
@@ -1,23 +1,39 @@
 package com.brick.service;
 
+import com.brick.common.FileStore;
 import com.brick.dto.UserRequestDto;
 import com.brick.dto.UserResponseDto;
 import com.brick.entity.User;
 import com.brick.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor // final 붙은 필드(userRepository)를 자동으로 생성자 주입
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
+    private final FileStore fileStore;
 
+    @Transactional
     @Override
     // 사용자가 입력한 데이터를 id로 찾는다
-    public void updateUser(Long userId, UserRequestDto dto) {
+    public void updateUser(Long userId, UserRequestDto dto ,MultipartFile image) {
         // userRepository(db의 창구)에서 id를 찾아 사용자가 입력한 데이터 (dto)를 user (entity) 실제 DB에 넣는다
         User user = userRepository.findById(userId)
-                .orElseThrow(()-> new RuntimeException("User not  found"));
+                .orElseGet(() -> User.builder().userId(userId).build());
+        // 닉네임 중복 체크
+        if (dto.getNickName() != null &&
+                userRepository.existsByNickNameAndUserIdNot(dto.getNickName(), userId)) {
+            throw new RuntimeException("이미 사용 중인 닉네임입니다.");
+        }
+
+        // 이미지 저장
+        if (image != null && !image.isEmpty()) {
+            String imageUrl = fileStore.save(image);
+            user.setImageUrl(imageUrl);
+        }
         user.setRealName(dto.getRealName());
         user.setAge(dto.getAge());
         user.setCity(dto.getCity());
@@ -25,8 +41,8 @@ public class UserServiceImpl implements UserService{
         user.setNickName(dto.getNickName());
         user.setMbti(dto.getMbti());
         user.setIntro(dto.getIntro());
-        user.setImageUrl(dto.getImageUrl());
 
+        userRepository.save(user);
     }
 
     @Override
