b2185443fd81c3f4894b13da72114d20c85e293a
Chaeyeoncho <gcy9293@naver.com>
2025-12-21 19:08:57 +0900

    refacotr : 이미지

--- src/main/java/com/brick/config/WebConfig.java
+++ src/main/java/com/brick/config/WebConfig.java
@@ -3,21 +3,39 @@ package com.brick.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
 
+    private static final String UPLOAD_DIR = "file:uploads/"; // 프로젝트 루트/uploads 폴더
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
+
+            //CORS 설정
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                // API
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173" ,"http://localhost:8080")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); // JWT 쓸 때 중요
+                        .allowCredentials(true);
+                // 이미지 파일
+                registry.addMapping("/uploads/**")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:8080")
+                        .allowedMethods("GET", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/uploads/**")
+                        .addResourceLocations(UPLOAD_DIR);
             }
         };
     }
